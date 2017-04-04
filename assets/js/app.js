1b91e53bf9edcafe27dab49d746196d6bff74e5f
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-04 10:56:15 -0500

    Arreglando otras cosas

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,8 @@ select.onchange = function(){
     document.getElementById("lima-5").style.display = "inline";
     document.getElementById("lima-6").style.display = "none";
   }else if (select.value == "lima-6") {
-    document.getElementById("chile-4").style.display = "none";
     document.getElementById("lima-5").style.display = "none";
+    document.getElementById("lima-").style.display = "none";
     document.getElementById("lima-6").style.display = "inline";
   }
 }
